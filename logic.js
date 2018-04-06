c4db81a2699bc0ec479fa797e0bdc49c3f693db4
Ramirez <0265888@students.dadeschools.net>
2018-04-06 13:14:59 -0400

    Hi

--- logic.js
+++ logic.js
@@ -1,28 +1,110 @@
-var FruitsList = ["banana","strawberry","apple"];
+var foodList = ["kelpjuice", "kelpfries", "krabbypatty", "krustykrabpizza"];
 
-var chosenword = '';
+var chosenWord = '';
 
-var letterInChosenword = [];
+var lettersInChosenWord= [];
 
-var numblanks = 0;
+var numBlanks = 0;
 
 var blanksAndSuccesses = [];
 
-var wrongGuesses = '';
+var wrongGuesses = [];
+
+var letterGuessed = '';
 
 
 var winCounter = 0;
 var lossCounter = 0;
-var numGuesses = 0;
-
-
-
+var numGuesses = 5;
 
 
 function startGame() {
-	numGuesses = 0;
-	chosenword = FruitsList[Math.floor(Math.random() = FruitsList.length)]
-	letterInChosenword = chosenword.slit("");
-	numblanks = letterInChosenword.length;
-	console.log(chosenword)
+
+	numGuesses = 5;
+
+	chosenWord = foodList[Math.floor(Math.random() * foodList.length)];
+
+	lettersInChosenWord = chosenWord.split("");
+
+	numBlanks = lettersInChosenWord.length;
+
+	console.log(chosenWord);
+
+	blanksAndSuccesses = [];
+
+	wrongGuesses = [];
+
+	for (var i = 0; i < numBlanks; i++) {
+		blanksAndSuccesses.push("_");
+	}
+
+	console.log(blanksAndSuccesses);
+
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
 }
+
+function checkLetters(letter) {
+	var letterInWord = false;
+
+	for (var i=0; i < numBlanks; i++) {
+		if (chosenWord[i] === letter) {
+			letterInWord = true;
+		}
+	}
+
+
+	if (letterInWord) {
+		for (var j=0; j < numBlanks; j++) {
+			if (chosenWord[j] === letter) {
+				blanksAndSuccesses[j] = letter;
+			}
+		}
+		console.log(blanksAndSuccesses);
+	} 
+
+	else {
+		wrongGuesses.push(letter);
+		numGuesses--;
+	}
+}
+
+function roundComplete() {
+	console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + 
+		" | NumGuesses: " + numGuesses);
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+	if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+		winCounter++;
+		alert("You win!");
+
+		document.getElementById("win-counter").innerHTML = winCounter;
+
+		startGame();
+	}
+
+	else if (numGuesses === 0) {
+		lossCounter++;
+		alert("You lose");
+
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+
+		startGame();
+	}
+}
+
+startGame();
+
+document.onkeyup = function(event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
+};
